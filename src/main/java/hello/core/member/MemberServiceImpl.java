9bb51c0072c6ebe5bb2cc2e36abba257ef047f0b
sheep-june <jun8547@gmail.com>
2025-08-03 15:49:18 +0900

    test 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
