5709dab3493e02dc7e4da4ecbd45584d34131704
sheep-june <jun8547@gmail.com>
2025-08-04 00:44:16 +0900

    할인정책 단일체계 원칙

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+//order클래스에서는 할인은 잘 모르겠고 니가 알아서 던져주셈 -> 단일체계 원칙 지키면서 설계
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        //회원정보 체크
+        Member member = memberRepository.findById(memberId);
+
+        //할인정책
+        //미래 확장성 생각해서 member자체를 넘김
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
