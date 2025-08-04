881dace0ac6776a931564b70011e902f246e1c0f
sheep-june <jun8547@gmail.com>
2025-08-04 14:09:54 +0900

    할인정책 변경(로직 변경 및 테스트)

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,41 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("vip는 10프로 할인 적용 되어야 함")
+    void vip_o(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    //오류 메세지 확인해보면 기댓값 1000원 하지만 아니였다 -> 체크
+    @DisplayName("vip가 아니면 할인 적용 안되어야 함")
+    void vip_x(){
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+}
