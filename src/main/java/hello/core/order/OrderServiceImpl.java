3693a9806e44f963f7fe0d16789632d8bdc9f22b
sheep-june <jun8547@gmail.com>
2025-08-05 10:44:57 +0900

    ocp dip위반

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,9 +10,13 @@ import hello.core.member.MemoryMemberRepository;
 //order클래스에서는 할인은 잘 모르겠고 니가 알아서 던져주셈 -> 단일체계 원칙 지키면서 설계
 
 public class OrderServiceImpl implements OrderService{
-
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //위에 주석코드는 ocp dip위반
+
+    private DiscountPolicy discountPolicy;
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
